bbbfcfa455e9bbd58e006bac83a17e680d205ab5
samuel-yang <samyang1995@gmail.com>
2017-12-09 16:30:43 -0500

    Fixing dialogue
    hopeful fix?

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -3,93 +3,113 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Dialogue : MonoBehaviour {
+public class Dialogue : MonoBehaviour
+{
 
-	private GameObject triggeringNpc;
-	private bool triggering;
+    private GameObject triggeringNpc;
+    private bool triggering;
 
-	public GameObject npcText;
-	public Text changeText;
-	public Text questText;
-	public Text sideQuestText;
+    public GameObject npcText;
+    public Text changeText;
+    public Text questText;
+    public Text sideQuestText;
 
-	public int countNextDialogue = 0;
+    public int countNextDialogue = 0;
 
-	void Start(){
+    void Start()
+    {
 
-	}
+    }
 
-	void Update(){
-		if (triggering) {
-			//print ("Player is triggering with " + triggeringNpc);
-			npcText.SetActive (true);
-			if (Input.GetKeyDown (KeyCode.T)) {
-				if (triggeringNpc.tag == "NPC1") {
-					if (countNextDialogue < 4) { 
-						changeText.text = "Hello hero! Follow me! A mother needs help!";
-						questText.text = "Active Quest: Follow Guard";
-						sideQuestText.text = "Side Quest: Get Information";
-					}
-					if (countNextDialogue > 3) { 
-						changeText.text = "Follow me hero! I will lead the way!";
+    void Update()
+    {
+        if (triggering)
+        {
+            //print ("Player is triggering with " + triggeringNpc);
+            npcText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                if (triggeringNpc.tag == "NPC1")
+                {
+                    if (countNextDialogue < 4)
+                    {
+                        changeText.text = "Hello hero! Follow me! A mother needs help!";
+                        questText.text = "Active Quest: Follow Guard";
+                        sideQuestText.text = "Side Quest: Get Information";
+                    }
+                    if (countNextDialogue > 3)
+                    {
+                        changeText.text = "Follow me hero! I will lead the way!";
 
-					}
-				}
-				if (triggeringNpc.CompareTag ("gate")) {
-					changeText.text = "I hope you bring that baby back ranger!";
-				}
-				if (triggeringNpc.CompareTag ("march")) {
-					changeText.text = "The night watch honors your bravery.";
-				}
-				if (triggeringNpc.tag == "NPC2") {
-					changeText.text = "Stop bothering me! Go away!";
-				}
-				if (triggeringNpc.tag == "NPC3") {
-					changeText.text = "Poor lady. I can't believe she lost her child to the beasts.";
-				}
-				if (triggeringNpc.tag == "NPC4") {
-					changeText.text = "I heard the beasts  love apples.";
-					sideQuestText.text = "Side Quest: Find Apples";
-				}
-				if (triggeringNpc.tag == "NPC5") {
-					changeText.text = "I get apples every morning from the apple tree on the mountain.";
-				}
-				if (triggeringNpc.tag == "NPC6") {
-					changeText.text = "Please help me! My child was taken away by a pack of beasts! Please bring him back to me!";
-					questText.text = "Active Quest: Save Child";
-				}
-				if (triggeringNpc.tag == "NPC7") {
-					changeText.text = "I am a wizard! I love magic!";
-				}
-				if (triggeringNpc.tag == "NPC8") {
-					changeText.text = "Don't mess with me! I'll smash you to bits!";
-				}
-				if (triggeringNpc.tag == "NPC9") {
-					changeText.text = "Left, right, right. Left, right, right. They say the path is fastest when you go left, right right.";
-				}
-			} else {
-				npcText.SetActive (false);
-			}
-		}
-			
-	}
+                    }
+                }
+                if (triggeringNpc.CompareTag("gate"))
+                {
+                    changeText.text = "I hope you bring that baby back ranger!";
+                }
+                if (triggeringNpc.CompareTag("march"))
+                {
+                    changeText.text = "The night watch honors your bravery.";
+                }
+                if (triggeringNpc.tag == "NPC2")
+                {
+                    changeText.text = "Stop bothering me! Go away!";
+                }
+                if (triggeringNpc.tag == "NPC3")
+                {
+                    changeText.text = "Poor lady. I can't believe she lost her child to the wolves.";
+                }
+                if (triggeringNpc.tag == "NPC4")
+                {
+                    changeText.text = "I heard wolves love apples.";
+                    sideQuestText.text = "Side Quest: Find Apples";
+                }
+                if (triggeringNpc.tag == "NPC5")
+                {
+                    changeText.text = "I get apples every morning from the apple tree on the mountain.";
+                }
+                if (triggeringNpc.tag == "NPC6")
+                {
+                    changeText.text = "Please help me! My child was taken away by a pack of wolves! Please bring him back to me!";
+                    questText.text = "Active Quest: Save Child";
+                }
+                if (triggeringNpc.tag == "NPC7")
+                {
+                    changeText.text = "I am a wizard! I love magic!";
+                }
+                if (triggeringNpc.tag == "NPC8")
+                {
+                    changeText.text = "Don't mess with me! I'll smash you to bits!";
+                }
+            }
+        }
+        else
+        {
+            npcText.SetActive(false);
+        }
 
-	void OnTriggerEnter(Collider other){
-		if (other.tag == "NPC1" || other.tag == "NPC2"|| other.tag =="NPC3"|| other.tag =="NPC4"||other.tag == "NPC5"|| other.tag =="NPC6"||
-			other.tag =="NPC7" || other.CompareTag("gate") || other.CompareTag("march") || other.CompareTag("NPC8") ) {
-			triggering = true;
-			triggeringNpc = other.gameObject;
-		}
-	}
+    }
 
-	void OnTriggerExit(Collider other){
-		if (other.tag == "NPC1" || other.tag =="NPC2"|| other.tag =="NPC3"||other.tag == "NPC4"|| other.tag =="NPC5"|| other.tag =="NPC6"||
-			other.tag =="NPC7" || other.CompareTag("gate") || other.CompareTag("march") || other.CompareTag("NPC8")) {
-			triggering = false;
-			triggeringNpc = null;
-			changeText.text = "Press T to Talk";
-			countNextDialogue++;
-		}
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "NPC1" || other.tag == "NPC2" || other.tag == "NPC3" || other.tag == "NPC4" || other.tag == "NPC5" || other.tag == "NPC6" ||
+            other.tag == "NPC7" || other.CompareTag("gate") || other.CompareTag("march") || other.CompareTag("NPC8"))
+        {
+            triggering = true;
+            triggeringNpc = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "NPC1" || other.tag == "NPC2" || other.tag == "NPC3" || other.tag == "NPC4" || other.tag == "NPC5" || other.tag == "NPC6" ||
+            other.tag == "NPC7" || other.CompareTag("gate") || other.CompareTag("march") || other.CompareTag("NPC8"))
+        {
+            triggering = false;
+            triggeringNpc = null;
+            changeText.text = "Press T to Talk";
+            countNextDialogue++;
+        }
+    }
 
 }
